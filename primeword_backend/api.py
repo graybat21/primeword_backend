970e5ec5b2ca4da77074e6cbc9130fe0ff70f45e
graybat21 <graybat21@gmail.com>
2018-03-13 21:07:45 +0900

    (진행중) (IssueID #001) Backend 개발환경 준비 ■ 수정사항: - testlog, memory 추가 ■ 수행한 개발자 테스트: - 단위테스트 ■ 미구현 사항 및 알고있는 이슈: - 없음

--- primeword_backend/api.py
+++ primeword_backend/api.py
@@ -14,10 +14,10 @@ textbooks_router.register(
     'notes', NoteViewSet,
     base_name='textbook-note',
     parents_query_lookups=['textbook']) \
-#     .register(
+    #     .register(
 #     'words', WordViewSet,
 #     base_name='textbook-note-word',
-#     parents_query_lookups=['note','word']
+#     parents_query_lookups=['textbook__note', 'word']
 # )
 
 # router.register('authors', AuthorViewSet)
@@ -26,5 +26,7 @@ notes_router.register(
     'words', WordViewSet,
     base_name='note-word',
     parents_query_lookups=['note']
+    # base_name='textbook-note-word',
+    # parents_query_lookups=['textbook__note', 'word']
 )
 router.register('words', WordViewSet)
--- primeword_backend/settings.py
+++ primeword_backend/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_swagger',
     'word',
+    'testlog',
+    'memory',
 ]
 
 MIDDLEWARE = [
