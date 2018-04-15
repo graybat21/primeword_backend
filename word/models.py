cee89fd1e7b20484ab550c38c0a8d5b5885b937c
graybat21 <graybat21@gmail.com>
2018-04-15 17:21:36 +0900

    (진행중) (IssueID #102) textbook, note 의 pk 를 uuid 로 바꾸기 ■ 수정사항: - 두 모델의 pk 를 int 타입 id 에서 UUID 타입 uuid 로 변경 ■ 수행한 개발자 테스트: - 단위테스트 ■ 미구현 사항 및 알고있는 이슈: - 없음

--- word/models.py
+++ word/models.py
@@ -1,9 +1,12 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 # Create your models here.
 class Textbook(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     grade = models.CharField(max_length=20)
     user = models.ForeignKey('auth.User')
@@ -14,6 +17,7 @@ class Textbook(models.Model):
 
 
 class Note(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     textbook = models.ForeignKey(Textbook)
     lesson = models.CharField(max_length=20)
     regdate = models.DateTimeField(auto_now_add=True)
@@ -22,17 +26,20 @@ class Note(models.Model):
         return self.lesson
 
 
+class Wordclass(models.Model):
+    name = models.CharField(max_length=20)
+    memo = models.TextField(blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Word(models.Model):
     word = models.CharField(max_length=100)
     meaning = models.TextField(max_length=500)
     note = models.ForeignKey(Note)
     regdate = models.DateTimeField(auto_now_add=True)
+    wordclass = models.ForeignKey(Wordclass)
 
     def __str__(self):
         return str(self.pk)
-
-# class User(AbstractUser):
-#     school = models.CharField(max_length=100)
-#     grade = models.CharField(max_length=20)
-#     def __str__(self):
-#         return f'{self.username}'
